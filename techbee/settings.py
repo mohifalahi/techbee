ea3336614ce469e5d91ebe7f59b3b073c11a9b40
Mohammad <mohifalahi@gmail.com>
2020-01-27 18:52:45 +0330

    settings file changed.

--- techbee/settings.py
+++ techbee/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -129,6 +130,6 @@ STATIC_URL = '/static/'
 # MEDIA_URL = '/media/'
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
