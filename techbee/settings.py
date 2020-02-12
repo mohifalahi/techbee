b942d049ae74ebe138cee191c6a73f9c2bb7801b
Mohammad <mohifalahi@gmail.com>
2020-02-12 18:46:29 +0330

    change static_root

--- techbee/settings.py
+++ techbee/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
 STATIC_URL = '/static/'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
