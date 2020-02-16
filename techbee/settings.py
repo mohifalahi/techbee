107b899a28c99d11af00ea15d64022fb1985a71b
Mohammad <mohifalahi@gmail.com>
2020-02-16 12:48:43 +0330

    adding staticfiles_dir

--- techbee/settings.py
+++ techbee/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
