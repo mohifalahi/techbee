caa392de33c2b8d38c05ef615e39730f2df29b70
Mohammad <mohifalahi@gmail.com>
2020-02-17 13:59:47 +0330

    change staticfiles path

--- techbee/settings.py
+++ techbee/settings.py
@@ -121,13 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'app/static')
-# ]
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app/myApp/static')
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
