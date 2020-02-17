7c4aa164765552c6a8cd0838f92c23f65d05ef55
Mohammad <mohifalahi@gmail.com>
2020-02-17 14:09:20 +0330

    rollback to the same staticfiles location

--- techbee/settings.py
+++ techbee/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/myApp/static')
+    os.path.join(BASE_DIR, 'app/static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
