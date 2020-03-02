ef239c939ca3522ce45cd8bc73cc200835f8f338
Mohammad <mohifalahi@gmail.com>
2020-03-02 11:41:37 +0330

    ajax updating issue solved.

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('', views.home, name='home'),
-    path('', views.submit_form, name='post')
+    path('', views.home, name='dashboard'),
+    path('devices', views.submit_form, name='devices'),
+    path('charts', views.chart, name='charts')
 ]
--- app/views.py
+++ app/views.py
@@ -2,34 +2,27 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Place
 
-# def home(request):
-#     places = Place.objects
-#     return render(request, 'index.html',{'places':places})
+def home(request):
+    places = Place.objects
+    return render(request, 'index.html',{'places':places})
 
 def submit_form(request):
     places = Place.objects
     if request.method == 'POST':
-        # if request.POST.get('button'):
-        #     post = Place()
-        #     post.place = request.POST.get('place')
-        #     post.device_type = request.POST.get('device_type')
-        #     post.device = request.POST.get('device')
-        #     post.unique_id = request.POST.get('unique_id')
-        #     post.save()
-        #     return render(request, 'index.html')
-        # else:
-        #     return render(request, 'index.html/admin')
-
         place = request.POST['place']
         device_type = request.POST['device_type']
         device = request.POST['device']
         unique_id = request.POST['unique_id']
+        icon_class = request.POST['icon_class']
 
         Place.objects.create(
             place = place,
             device_type = device_type,
             device = device,
-            unique_id = unique_id
+            unique_id = unique_id,
+            icon_class = icon_class
         )
+    return render(request, 'devices.html',{'places':places})
 
-    return render(request, 'index.html',{'places':places})
+def chart(request):
+    return render(request, 'charts.html')
